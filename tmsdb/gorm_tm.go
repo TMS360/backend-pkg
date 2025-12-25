c5952d2efbb58b5989ee6876322adfaa7841a276
Ravshan Davlatov <davlatovravshan014@gmail.com>
2025-12-25 18:58:55 +0500

    refactor: remove unused eventlog references and simplify Publish method implementation

--- tmsdb/gorm_tm.go
+++ tmsdb/gorm_tm.go
@@ -2,10 +2,7 @@ package tmsdb
 
 import (
 	"context"
-	"encoding/json"
-	"time"
-
-	"github.com/TMS360/backend-pkg/eventlog"
+	//"github.com/TMS360/backend-pkg/eventlog"
 	"github.com/TMS360/backend-pkg/events"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -40,21 +37,23 @@ func (m *GormTransactionManager) GetDB(ctx context.Context) *gorm.DB {
 
 // Publish implements the logic DIRECTLY here. No Repo.
 func (m *GormTransactionManager) Publish(ctx context.Context, aggID uuid.UUID, aggType, evtType string, payload events.EventPayload) error {
-	payloadBytes, err := json.Marshal(payload)
-	if err != nil {
-		return err
-	}
+	//payloadBytes, err := json.Marshal(payload)
+	//if err != nil {
+	//	return err
+	//}
 
-	event := eventlog.OutboxEvent{
-		ID:            uuid.New(),
-		AggregateID:   aggID,
-		AggregateType: aggType,
-		EventType:     evtType,
-		Payload:       payloadBytes,
-		Status:        "PENDING",
-		CreatedAt:     time.Now(),
-	}
+	return nil
 
-	// Uses the active transaction from context automatically
-	return m.GetDB(ctx).Create(&event).Error
+	//event := eventlog.OutboxEvent{
+	//	ID:            uuid.New(),
+	//	AggregateID:   aggID,
+	//	AggregateType: aggType,
+	//	EventType:     evtType,
+	//	Payload:       payloadBytes,
+	//	Status:        "PENDING",
+	//	CreatedAt:     time.Now(),
+	//}
+	//
+	//// Uses the active transaction from context automatically
+	//return m.GetDB(ctx).Create(&event).Error
 }

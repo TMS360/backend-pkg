0ec2ce354dceb9c0f97315f20d0556f09ff4dbfc
muhammadislom <islom.pr@gmail.com>
2026-02-07 12:48:19 +0500

    feat: modify CORS middleware to accept dynamic allowed origins and streamline configuration

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -1,27 +1,40 @@
 package middleware
 
 import (
-	"slices"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func Cors(allowedOrigins []string) gin.HandlerFunc {
+func Cors() gin.HandlerFunc {
 	cfg := cors.Config{
 		AllowOriginFunc: func(origin string) bool {
-			if slices.Contains(allowedOrigins, origin) {
-				return true
-			}
-			return false
+			return true
 		},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length", "Set-Cookie"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Accept", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}
-
 	return cors.New(cfg)
 }
+
+//func Cors(allowedOrigins []string) gin.HandlerFunc {
+//	cfg := cors.Config{
+//		AllowOriginFunc: func(origin string) bool {
+//			if slices.Contains(allowedOrigins, origin) {
+//				return true
+//			}
+//			return false
+//		},
+//		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+//		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+//		ExposeHeaders:    []string{"Content-Length", "Set-Cookie"},
+//		AllowCredentials: true,
+//		MaxAge:           12 * time.Hour,
+//	}
+//
+//	return cors.New(cfg)
+//}

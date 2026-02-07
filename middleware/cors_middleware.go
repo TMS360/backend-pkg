8a41471836b4d9d677e1d4c48b5e412025564352
muhammadislom <islom.pr@gmail.com>
2026-02-07 14:20:18 +0500

    feat: refactor CORS middleware to accept a list of allowed origins and remove commented code

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -7,20 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//func Cors() gin.HandlerFunc {
-//	cfg := cors.Config{
-//		AllowOriginFunc: func(origin string) bool {
-//			return true
-//		},
-//		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-//		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-//		ExposeHeaders:    []string{"Content-Length", "Set-Cookie"},
-//		AllowCredentials: true,
-//		MaxAge:           12 * time.Hour,
-//	}
-//	return cors.New(cfg)
-//}
-
 func Cors(allowedOrigins []string) gin.HandlerFunc {
 	cfg := cors.Config{
 		AllowOriginFunc: func(origin string) bool {

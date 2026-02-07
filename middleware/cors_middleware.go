581235b27e695feebb5644a607531627dc85c16b
muhammadislom <islom.pr@gmail.com>
2026-02-07 14:02:22 +0500

    feat: update CORS middleware to accept a list of allowed origins

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -1,16 +1,34 @@
 package middleware
 
 import (
+	"slices"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func Cors() gin.HandlerFunc {
+//func Cors() gin.HandlerFunc {
+//	cfg := cors.Config{
+//		AllowOriginFunc: func(origin string) bool {
+//			return true
+//		},
+//		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+//		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Accept", "X-Requested-With"},
+//		ExposeHeaders:    []string{"Content-Length"},
+//		AllowCredentials: true,
+//		MaxAge:           12 * time.Hour,
+//	}
+//	return cors.New(cfg)
+//}
+
+func Cors(allowedOrigins []string) gin.HandlerFunc {
 	cfg := cors.Config{
 		AllowOriginFunc: func(origin string) bool {
-			return true
+			if slices.Contains(allowedOrigins, origin) {
+				return true
+			}
+			return false
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "Accept", "X-Requested-With"},
@@ -18,23 +36,6 @@ func Cors() gin.HandlerFunc {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}
+
 	return cors.New(cfg)
 }
-
-//func Cors(allowedOrigins []string) gin.HandlerFunc {
-//	cfg := cors.Config{
-//		AllowOriginFunc: func(origin string) bool {
-//			if slices.Contains(allowedOrigins, origin) {
-//				return true
-//			}
-//			return false
-//		},
-//		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-//		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-//		ExposeHeaders:    []string{"Content-Length", "Set-Cookie"},
-//		AllowCredentials: true,
-//		MaxAge:           12 * time.Hour,
-//	}
-//
-//	return cors.New(cfg)
-//}

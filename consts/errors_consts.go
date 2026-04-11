5a516f250a638740721aff6c33c6138c48b87541
Ravshan Davlatov <ravshandavlatov014@gmail.com>
2026-04-11 17:36:25 +0500

    feat: add forbidden error constant for improved error handling

--- consts/errors_consts.go
+++ consts/errors_consts.go
@@ -6,5 +6,6 @@ var (
 	ErrValidation         = response.NewError("validation_error", "Validation Error", 400)
 	ErrInvalidCredentials = response.NewError("invalid_credentials", "Invalid Credentials", 400)
 	ErrUnauthorized       = response.NewError("unauthorized", "Unauthorized", 401)
+	ErrForbidden          = response.NewError("forbidden", "Forbidden", 403)
 	ErrInvalidRequestBody = response.NewError("invalid_request_body", "Invalid Request Body", 400)
 )

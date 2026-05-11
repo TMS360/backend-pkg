73ade070f8910c049f2f961e28748939783b7802
Ravshan Davlatov <davlatovravshan014@gmail.com>
2026-05-11 12:35:04 +0500

    feat: add additional user roles to user_roles.go

--- enums/user_roles.go
+++ enums/user_roles.go
@@ -23,12 +23,24 @@ func (s UserRoleEnum) String() string {
 		return "super_admin"
 	case UserRoleAdmin:
 		return "admin"
+	case UserRoleManager:
+		return "manager"
+	case UserRoleAccounting:
+		return "accounting"
+	case UserRoleSafety:
+		return "safety"
+	case UserRoleFleet:
+		return "fleet"
+	case UserRoleHr:
+		return "hr"
 	case UserRoleDispatcher:
 		return "dispatcher"
 	case UserRoleDriver:
 		return "driver"
 	case UserRoleCustomer:
 		return "customer"
+	case UserRoleOther:
+		return "other"
 	default:
 		return "unknown"
 	}
@@ -37,7 +49,7 @@ func (s UserRoleEnum) String() string {
 // IsValid checks if the status is a known value
 func (s UserRoleEnum) IsValid() bool {
 	switch s {
-	case UserRoleSuperAdmin, UserRoleAdmin, UserRoleDispatcher, UserRoleDriver, UserRoleCustomer:
+	case UserRoleSuperAdmin, UserRoleAdmin, UserRoleManager, UserRoleAccounting, UserRoleSafety, UserRoleFleet, UserRoleHr, UserRoleDispatcher, UserRoleDriver, UserRoleCustomer, UserRoleOther:
 		return true
 	default:
 		return false

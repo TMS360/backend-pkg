bcb9b824308f07985e2ac0866be50a17b788d103
Ravshan Davlatov <davlatovravshan014@gmail.com>
2026-06-03 14:08:50 +0500

    update PostgreSQL connection settings to improve performance

--- client/postgresql/postgresql.go
+++ client/postgresql/postgresql.go
@@ -68,7 +68,8 @@ func NewClient(cfg config.PostgresSQLConfig) (*gorm.DB, error) {
 	if sqlDB, err := db.DB(); err == nil {
 		sqlDB.SetMaxIdleConns(10)
 		sqlDB.SetMaxOpenConns(100)
-		sqlDB.SetConnMaxLifetime(time.Hour)
+		sqlDB.SetConnMaxLifetime(30 * time.Minute)
+		sqlDB.SetConnMaxIdleTime(5 * time.Minute)
 		//sqlDB.SetMaxIdleConns(5)
 		//sqlDB.SetMaxOpenConns(20)
 		//sqlDB.SetConnMaxLifetime(30 * time.Minute)
